13541741ae75cdfcd7c260b32fd95bc1c42178e4
skdesignoweb <shivanshkatiyardesignoweb@gmail.com>
2025-10-08 18:34:17 +0530

    zoho sync api

--- appsail-nodejs/src/models/user.model.js
+++ appsail-nodejs/src/models/user.model.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt"); // official bcrypt
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: { type: String },
     lastName: { type: String },
     fullName: { type: String },
@@ -14,24 +15,26 @@ const userSchema = new mongoose.Schema({
     zohoUserId: { type: String, required: true, unique: true },
     visaRefused: { type: Boolean },
     passportNo: { type: String },
-    status: { type: String, default: "Active" }
-}, { timestamps: true });
+    status: { type: String, default: "Active" },
+  },
+  { timestamps: true }
+);
 
 // Hash password before saving
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 // Compare entered password with hashed password
 userSchema.methods.comparePassword = async function (enteredPassword) {
-    return bcrypt.compare(enteredPassword, this.password);
+  return bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model("User", userSchema);
